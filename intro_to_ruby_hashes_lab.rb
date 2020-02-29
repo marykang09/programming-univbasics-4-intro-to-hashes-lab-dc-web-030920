bd7efec8e4516e22c7501cef6598b0bb0349d9d9
Mary Kang <mary.kang09@gmail.com>
2020-02-29 16:42:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,9 +25,8 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
 new_hash = {
-  :"key" => value
+  :key => value
 }
-new_hash
 end
 
 def read_from_hash(hash, key)
