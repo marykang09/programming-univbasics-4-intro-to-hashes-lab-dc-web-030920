fd4178b3086c837de14ee661150a5e750c8eef87
Mary Kang <mary.kang09@gmail.com>
2020-02-29 16:41:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def id_generator
   }
 end
 
-def my_hash_creator(key, value)
+def my_hash_creator("key", value)
   # return a hash that includes the key and value parameters passed into this method
 new_hash = {
   :key => value
